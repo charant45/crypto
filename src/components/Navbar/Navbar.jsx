c64e83ee85bb0d4ab0955c7bb3e25261babf48aa
charan <charan4567890@gmail.com>
2024-06-30 19:37:22 +0530

    nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,32 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.svg'
-import crypto from '../../assets/Crypto.svg'
-import arrow_icon from '../../assets/arrow_icon.png'
+import React from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.svg';
+import crypto from '../../assets/Crypto.svg';
 
 const Navbar = () => {
-  return (
-    <div className='navber'>
-        <span><img src={logo} alt="img"/><img src={crypto} alt="img1"/></span>
-        <ul>
-            <li>Home</li>
-            <li>Features</li>
-            <li>Pricing</li>
-            <li>Blog</li>
-        </ul>
-        <div className='nav-right'>
-            <select>
-                <option value="usd">USD</option>
-                <option value="eur">EUR</option>
-                <option value="inr">INR</option>
-            </select>
-            <button>Sign Up<img src={arrow_icon} alt=""/></button>
-        </div>
-    </div>
-  )
-}
 
-export default Navbar
+  return (
+    <div className='navbar'>
+      <span className='logo-container'>
+        <img src={logo} alt="Logo" className='logo' />
+        <img src={crypto} alt="Crypto" className='img' />
+      </span>
+      <ul >
+        <li>Home</li>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className='nav-right'>
+        <select>
+          <option value='USD'>USD</option>
+          <option value='EUR'>EUR</option>
+          <option value='INR'>INR</option>
+        </select>
+      </div>
+      <button className='get-started-btn'>Get Started</button>
+    </div>
+  );
+};
+
+export default Navbar;
