b3753005a6c63810e5a4761e043b7e8eae7abc92
charan <charan4567890@gmail.com>
2024-07-08 15:51:48 +0530

    commit 3

--- src/components/Common/Header/index.js
+++ src/components/Common/Header/index.js
@@ -41,7 +41,7 @@ function Header() {
   return (
     <div className="header">
       <h1>
-        Crypto<span style={{ color: "var(--blue)" }}>.</span>
+        Crypto<span style={{ color: "var(--blue)" ,fontSize:"larger"}}>.</span>
       </h1>
       <div className="links">
         <Switch checked={darkMode} onClick={() => changeMode()} />
